657490116c3cbecb3588f482cd307ebe307a23f9
Bartosz Maćkiewicz <hprzezh@gmail.com>
2019-02-19 00:45:02 +0100

    aktualizacja

---
+++ 01_Korpusy_i_wyszukiwarki/03. Prezentacja/auto/prezentacja.el
@@ -0,0 +1,33 @@
+(TeX-add-style-hook
+ "prezentacja"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("ccicons" "scale=2") ("babel" "polish") ("inputenc" "utf8") ("fontenc" "T1")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"
+    "lmodern"
+    "ccicons"
+    "polski"
+    "babel"
+    "listings"
+    "listingsutf8"
+    "graphicx"
+    "smartdiagram"
+    "inputenc"
+    "fontenc"
+    "tabularx"))
+ :latex)
+
